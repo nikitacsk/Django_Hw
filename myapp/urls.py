a6a652f38fcd4e429643c0cb4aeec4e8af253a51
nikitacsk <nikita.voronkin3@gmail.com>
2024-07-23 22:12:10 +0200

    Hw_2

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,4 +1,3 @@
-
 from django.urls import path, re_path
 from . import views
 from myapp.views import index, acricles, acrticles_archive, users, article, article_slug, users_id
@@ -11,9 +10,9 @@ urlpatterns = [
     path('acricles/', acricles),
     path('acrticles/archive/', acrticles_archive),
     path('users/', users),
-    path('articles/<int:article_number>', article),
+    path('article/<int:article_number>', article, name='number'),
     path('article/<int:article_number>/archive', article_slug),
-    path('article/<int:article_number>/<slug:slug_text>', article_slug),
+    path('article/<int:article_number>/<slug:slug_text>', article_slug, name='slug'),
     path('users/<int:user_number>', users_id),
     re_path(r'^phone/(?P<phone_number>0(39|50|63|66|67|68|91|92|93|94|95|96|97|98|99)[0-9]{7})/$', views.phone_function, name='phone_number'),
     re_path(r'custom/(?P<year>[1-9a-f]{4}-[1-9a-f]{6})/$', views.custom_view),
--- myapp/views.py
+++ myapp/views.py
@@ -1,32 +1,75 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
+import random
+import string
 # Create your views here.
+
+
 def index(request):
-    return HttpResponse("Hello, world")
+
+    num = random.randint(1, 100)
+    length = random.randint(5, 10)
+    slug = ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
+    data = {
+        'num': num,
+        'slug': slug,
+    }
+
+    return render(
+        request,
+        'myapp/index.html',
+        data
+    )
+
 
 def my_url(request):
-    return HttpResponse("Hello it's my_url")
+    data = {'text': 'my_url'}
+    return render(request, 'myapp/static.html', data)
+
 
 def acricles(request):
-    return HttpResponse("Hello it's acricles")
+    data = {'text': 'acricles'}
+    return render(request, 'myapp/static.html', data)
+
 
 def acrticles_archive(request):
-    return HttpResponse("Hello it's acrticles_archive")
+    data = {'text': 'acrticles_archive'}
+    return render(request, 'myapp/static.html', data)
+
 
 def users(request):
-    return HttpResponse("Hello it's users")
+    data = {'text': 'users'}
+    return render(request, 'myapp/static.html', data)
+
 
 def article(request, article_number):
-    return HttpResponse(f"Hello it's article ")
+    data = {
+        'article_number': article_number,
+        'bool': article_number % 2
+            }
+    return render(
+        request,
+        'myapp/article_number.html',
+        data
+    )
+
 
 def article_slug(request, article_number, slug_text=''):
-    return HttpResponse(f"Hello it's article number {article_number}, text is {slug_text}")
+    data = {'article_number': article_number, 'slug_text': slug_text}
+    return render(
+        request,
+        'myapp/article_slug.html',
+        data
+    )
+
 
 def users_id(request, user_number):
     return HttpResponse(f"Hello it's users {user_number}")
 
+
 def phone_function(request, phone_number):
     return HttpResponse(f"Ваш номер телефону: {phone_number}")
+
 
 def custom_view(request, year):
     return HttpResponse(f"Ваш параметр: {year}")
