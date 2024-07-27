ceebd5a13501c9eda2e2146d00b4e67cd8750f98
nikitacsk <nikita.voronkin3@gmail.com>
2024-07-27 18:28:30 +0200

    Hw 4

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,8 +1,2 @@
 from django.contrib import admin
 
-from myapp.models import Catalog, Book, Author 
-
-# Register your models here.
-admin.site.register(Catalog)
-admin.site.register(Book)
-admin.site.register(Author)
--- myapp/models.py
+++ myapp/models.py
@@ -1,6 +1,6 @@
+from datetime import timedelta
+from django.utils import timezone
 from django.db import models
-
-# Create your models here.
 
 
 class Catalog(models.Model):
@@ -42,13 +42,20 @@ class Article(models.Model):
 
 
 class Comment(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
+    article = models.ForeignKey(Article, null=True, blank=True, on_delete=models.CASCADE, related_name='comments')
     parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='replies')
     author_name = models.CharField(max_length=255)
     content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"Comment by {self.author_name} on {self.article}"
+        return f"Comment by {self.author_name} text is {self.content}"
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.created_at = timezone.now() - timedelta(days=365)
+        super().save(*args, **kwargs)
 
 
 class ArticleLike(models.Model):
